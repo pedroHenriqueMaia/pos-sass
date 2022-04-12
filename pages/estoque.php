f4e36897cebc00160a93ce55aa001c8f6c00ec93
Pedro Henrique Maia <pedro.maia@expertisesolutions.com.br>
2022-04-11 23:19:52 -0300

    Initial commit

---
+++ pages/estoque.php
@@ -0,0 +1,8 @@
+<?php if ( !isset( $pagina ) ) exit; ?>
+<h1 class="main-center">Estoque:</h1>
+
+<div class="main">
+    <div class="main-grid-sobre-coluna">
+        <p>Estoque vazio!</p>
+    </div>
+</div>
